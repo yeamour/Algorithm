3675d58cc8015cd4ac6d77b295af2a348d3604b7
yeamour <ye_amour0203@126.com>
2017-06-25 01:02:46 +0800

    add UnitTest

---
+++ CommonAlgorithm/CommonAlgorithm/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = new int[] { 5, 8, 0, 4, 2, 7, 6, 1, 9, 3 };
+            CommonSort.InsertionSort(data);
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.WriteLine(data[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+    
+    public class CommonSort
+    {
+        public static bool BubbleSort(int[] data)
+        {
+            if (data == null || data.Length < 2)
+            {
+                return false;
+            }
+            for (int i = 0; i < data.Length - 1; i++)
+            {
+                for (int j = 0; j < data.Length - i - 1; j++)
+                {
+                    if (data[j] > data[j + 1])
+                    {
+                        Exchange(data, j, j + 1);
+                    }
+                }
+            }
+            return true;
+        }
+
+        public static bool SelectionSort(int[] data)
+        {
+            if(data == null || data.Length < 2)
+            {
+                return false;
+            }
+            for (int i = 0; i < data.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < data.Length; j++)
+                {
+                    if (data[min] > data[j])
+                    {
+                        min = j;
+                    }
+                }
+                if (min != i)
+                {
+                    Exchange(data, min, i);
+                }
+            }
+            return true;
+        }
+
+        public static bool InsertionSort(int[] data)
+        {
+            if (data == null || data.Length < 2)
+            {
+                return false;
+            }
+            for (int i = 1; i < data.Length; i++)
+            {
+                int num = data[i];
+                int j = i - 1;
+                while ( j >= 0 && data[j] > num)
+                {
+                    data[j + 1] = data[j];
+                    j--;
+                }
+                data[j + 1] = num;
+            }
+            return true;
+        }
+
+        private static void Exchange(int[] data, int i, int j)
+        {
+            int tmp = data[i];
+            data[i] = data[j];
+            data[j] = tmp;
+        }
+    }
+
+}
---
+++ CommonAlgorithm/UnitTest/CommonSort_UnitTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CommonAlgorithm;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class CommonSort_UnitTest
+    {
+        int[] data = new int[] { 5, 8, 0, 4, 2, 7, 6, 1, 9, 3 };
+        int[] rightData = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        [TestMethod]
+        public void BubbleSort_asc_true()
+        {
+            CommonSort.BubbleSort(data);
+            Assert.IsTrue(Compare(data, rightData));
+        }
+        [TestMethod]
+        public void SelectionSort_asc_true()
+        {
+            CommonSort.SelectionSort(data);
+            Assert.IsTrue(Compare(data, rightData));
+        }
+        [TestMethod]
+        public void InsertionSort_asc_true()
+        {
+            CommonSort.InsertionSort(data);
+            Assert.IsTrue(Compare(data, rightData));
+        }
+
+        bool Compare(int[] data, int[] rightData)
+        {
+            if (data == null || data.Length != rightData.Length)
+            {
+                return false;
+            }
+            for (int i = 0; i < rightData.Length; i++)
+            {
+                if (rightData[i] != data[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
